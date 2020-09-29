2dc2b11e7e569c9f48924b1c1ab6e50ec2c32400
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-29 17:50:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,50 @@
+require 'pry'
+
+# name = wine
+# collection = [{:item=>"DOG FOOD"}, {:item=>"WINE"}, {:item=>"STRYCHNINE"}]
+# key = {:item => "DOG FOOD"}
+
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  index = 0
+  
+  collection.each do |gitem| 
+    return gitem if gitem[:item] == name
+    index += 1
 
 end
+nil
+end
+
+
+
+# cart = [{:item=>"TEMPEH", :price=>3.0, :clearance=>true},
+# {:item=>"PEANUTBUTTER", :price=>3.0, :clearance=>true},
+# {:item=>"ALMONDS", :price=>9.0, :clearance=>false}]
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  index = 0
+  new_cart = []
 
+  while index < cart.length 
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item != nil
+      current_item[:count] += 1
+    else current_item = {
+      :item => current_item[index][:item],
+      :price => current_item[index][:price],
+      :clearance => current_item[index][:clearance],
+      :count => current_item[index][:count]
+    }
+      new_cart << current_item
+      
+
+
+
+end
+index += 1
+end
+new_cart
 end
 
 
