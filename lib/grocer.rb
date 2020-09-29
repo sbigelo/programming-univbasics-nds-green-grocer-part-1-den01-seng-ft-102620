dd2e919fba8d28aa6e13221660f0d5240c1ee7bc
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-29 17:54:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -38,6 +38,28 @@ def consolidate_cart(cart)
     }
       new_cart << current_item
       
+def consolidate_cart(cart)
+  index = 0
+  new_cart = []
+  
+  while index < cart.length do
+    item = find_item_by_name_in_collection(cart[index][:item], new_cart)
+    if item
+      new_cart_index = 0
+      while new_cart_index < new_cart.length do
+        if new_cart[new_cart_index][:item] === item[:item]
+          new_cart[new_cart_index][:count] += 1
+        end
+        new_cart_index += 1
+      end
+    else
+      cart[index][:count] = 1
+      new_cart << cart[index]
+    end
+    index += 1
+  end
+  new_cart
+end
 
 
 
